7c725443cc5ab94cab11e26ec43a57b9815eeaca
Alexander Ageyev <s94827@yandex.ru>
2021-08-17 20:35:43 +0500

    __save.bat update

--- __save.bat
+++ __save.bat
@@ -1,7 +1,7 @@
 DEL /S /Q RESULT > nul
 
 if exist src.7z del src.7z
-7za.exe a -r src.7z *.sln *.cs *.resx *.csproj *.user *.ico *.bat sfxcfg.txt app.config LICENSE README.md
+7za.exe a -r src.7z *.sln *.cs *.resx *.csproj *.user *.ico *.bat sfxcfg.txt app.config LICENSE README.md .gitignore
 7za.exe a src.7z *.sfx *.txt
 
 if exist exe.7z del exe.7z
